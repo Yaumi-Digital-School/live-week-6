d0ca0d0e2fa737e824e6a341da6c8f4b81877a50
MyukaQA <mylian214@gmail.com>
2021-10-30 10:00:49 +0700

    change task

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,5 +57,10 @@
 
 </html>
 <script>
-
+$(document).ready(function() {
+    $('.task-checkbox').click(function() {
+        taskId = $(this).val();
+        window.location.href = "/todo/change/" + taskId;
+    })
+})
 </script>
