4d9db76fa838c8a028342ec5effe982ecda4b9f8
aisyahm <aisyahmulyani01@gmail.com>
2021-10-30 09:26:20 +0700

    aisyah trying to edit index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,15 @@
 <!-- INSERT YOUR TODO CODE HERE -->
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+// require_once __DIR__ . '/vendor/autoload.php';
 
-use app\model\Task;
+use app\Models\Task;
 
-$tasks = Task::getAll();
+$tasks = new Task();
 ?>
 <html>
 
 <head>
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" href="/app.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
@@ -33,12 +33,12 @@ $tasks = Task::getAll();
             <div class="section-task">
                 <!-- TODO: Section task hanya berisi task yang belum done (is_done = 0) -->
                 <?php foreach ($tasks as $task) {
-                    if ($task->isDone == 0) {
+                    if ($task['is_done'] == 0) {
                 ?>
                         <div class="task-item">
                             <input type="checkbox" name="todos_item[]" class="task-checkbox" value="<?php echo $task->id ?>" id="todo-<?php echo $task->id ?>" />
 
-                            <span class="<?php if ($task->isDone == 1) echo "mark-done" ?>"><?php echo $task->name; ?></span>
+                            <span class="<?php if ($task['is_done'] == 1) echo "mark-done" ?>"><?php echo $task->name; ?></span>
                             </span>
                             <form action="remove_task.php" method="POST">
                                 <button type="submit" name="remove" value="<?php echo $task->id ?>" class="remove-button">X</button>
@@ -53,7 +53,7 @@ $tasks = Task::getAll();
             <div class="section-task-done">
                 <h4>Completed</h4>
                 <?php foreach ($tasks as $task) {
-                    if ($task->isDone == 1) { ?>
+                    if ($task['is_done'] == 1) { ?>
                         <div class="task-item" style="margin: 20px 0; border: solid 1px cornflowerblue">
                             <input type="checkbox" name="todos_item[]" class="task-checkbox" value="<?php echo $task->id ?>" checked id="todo-<?php echo $task->id ?>" />
                             <span class="mark-done"><?php echo $task->name; ?></span>
